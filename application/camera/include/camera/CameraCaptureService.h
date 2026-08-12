5fb3f8cbe49672439a287da917c1eafeb90a0b2f
yingqihang <yingqihang@femtomarch.com>
2026-08-12 18:06:53 +0800

    增加promise+future异步回复控制命令结果的功能

--- application/camera/include/camera/CameraCaptureService.h
+++ application/camera/include/camera/CameraCaptureService.h
@@ -1,12 +1,13 @@
 #pragma once
 #include <camera/ICameraDevice.h>
-#include <memory>
-#include <string>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <queue>
+#include <future>
 #include <atomic>
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+#include <functional>
+#include <memory>
 
 namespace learnopengl::application {
 
@@ -15,42 +16,55 @@ public:
     explicit CameraCaptureService(std::unique_ptr<ICameraDevice> cameraDevice);
     ~CameraCaptureService();
 
-
     CameraCaptureService(const CameraCaptureService &) = delete;
-    CameraCaptureService& operator=(const CameraCaptureService&) = delete;
+    CameraCaptureService& operator=(const CameraCaptureService &) = delete;
     CameraCaptureService(CameraCaptureService &&) = delete;
     CameraCaptureService& operator=(CameraCaptureService &&) = delete;
+
+    void run(std::unique_ptr<ICameraDevice> cameraDevice);
+    void shutdown();
 
 public:
     enum class State {
         Closed,
-        Opened,
-        Capturing
+        Captured,
+        Opened
     };
-    State state() const noexcept;
-    bool requestOpen();
-    bool requestClose();
-    bool requestStartCapture();
-    bool requestStopCapture();
-    bool requestSetFrameCallback(ICameraDevice::FrameCallback callback);
+    State state() const;
 
-    void shutdown();
-
-private:
-    using Command = std::function<void(ICameraDevice &cameraDevice)>;
-    bool post(Command command);
-    void run(std::unique_ptr<ICameraDevice> cameraDevice);
+    std::future<CameraResult> requestOpen();
+    std::future<CameraResult> requestClose();
+    std::future<CameraResult> requestStartCapture();
+    std::future<CameraResult> requestStopCaptrue();
+    std::future<CameraResult> requestSetFrameCallback(ICameraDevice::FrameCallback callback);
 
 private:
+    // 控制命令动作
+    using CommandAction = std::function<CameraResult(ICameraDevice &device)>;
+    
+    // 控制命令，包含命令动作以及异步的控制结果
+    struct Command {
+        CommandAction action;  
+        std::promise<CameraResult> promiseResult; // 异步命令结果，用std::promise写入
+    };
+
+    // 将控制命令动作和std::promise一起打包到队列中，控制线程会取出来操作，CameraCaptureService所在线程可取得对应的future对象。
+    std::future<CameraResult> submit(CommandAction action);
+    
+private:
+    // 锁保护请求退出标志位以及队列
     std::mutex m_mutex;
     std::condition_variable m_condition;
+    bool m_shutdownRequest{false};
     std::queue<Command> m_commands;
-    std::atomic<State> m_state{State::Closed};
-    bool m_shutdownRequested{false};
 
-    std::thread m_controlThread;
+    // 设备状态用原子量，CameraCaptureService也可以知道当前设备状态
+    std::atomic<State> m_state{State::Closed};
+    std::thread m_thread;
+
 
 };
+
 
 
 
--- application/camera/src/CameraCaptureService.cpp
+++ application/camera/src/CameraCaptureService.cpp
@@ -1,181 +1,198 @@
 #include <camera/CameraCaptureService.h>
-#include <stdexcept>
-#include <utility>
 
 namespace learnopengl::application {
 
+namespace {
+
+CameraResult succeed() {
+    CameraResult result;
+    result.succeeded = true;
+    return result;
+}
+
+CameraResult failure(std::string errorMsg) {
+    CameraResult result;
+    result.succeeded = false;
+    result.errorMessage = errorMsg;
+    return result;
+}
+
+} // namespace
+
 CameraCaptureService::CameraCaptureService(std::unique_ptr<ICameraDevice> cameraDevice) {
     if (cameraDevice == nullptr) {
-        throw std::invalid_argument("require a camera device");
+        throw std::invalid_argument("构造函数无有效传递的相机设备对象");
     }
-
-    m_controlThread = std::thread(&CameraCaptureService::run, this, std::move(cameraDevice));
+    
+    // 构造函数中直接启动控制线程
+    m_thread = std::thread(&CameraCaptureService::run, this, std::move(cameraDevice));
 }
 
 CameraCaptureService::~CameraCaptureService() {
     shutdown();
 }
 
-bool CameraCaptureService::post(Command command) {
-    if (command == nullptr) {
-        return false;
+void CameraCaptureService::shutdown() {
+    // 请求停止状态置为true，并唤醒条件变量让线程准备退出
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_shutdownRequest = true;
     }
+    m_condition.notify_one();
 
+    // 等待线程结束
+    if (m_thread.joinable()) {
+        m_thread.join();
+    }
+}
+
+std::future<CameraResult> CameraCaptureService::submit(CommandAction action) {
+    // Command中包含命令以及std::promise<CameraResult>,需要在这里把promise和future建立共享关系。
+    Command command;
+    std::future<CameraResult> future = command.promiseResult.get_future();
+
+    if (action == nullptr) {
+        command.promiseResult.set_value(failure("命令为空指针"));
+        return future;
+    }
+    command.action = std::move(action);
+
+    // 准备将命令CommandAction投递到队列中，并唤醒控制线程进行处理
     {
         std::lock_guard<std::mutex> lock(m_mutex);
 
-        if (m_shutdownRequested == true) {
-            return false;
+        // 如果正在请求退出，则拒绝任何命令的提交
+        if (m_shutdownRequest == true) {
+            command.promiseResult.set_value(failure("正在请求退出，拒绝任何命令的加入"));
+            return future;
         }
-
         m_commands.push(std::move(command));
     }
     m_condition.notify_one();
-    return true;
-
+    return future;
 }
 
 void CameraCaptureService::run(std::unique_ptr<ICameraDevice> cameraDevice) {
-
     while (1) {
         Command command;
-        { // 开始临界区
+        {
             std::unique_lock<std::mutex> lock(m_mutex);
+            // 只有请求退出或者命令队列不为空时唤醒
             m_condition.wait(lock, [this]() {
-                return !m_commands.empty() || m_shutdownRequested;
+                return m_shutdownRequest || !m_commands.empty();
             });
-            
-            if (m_shutdownRequested && m_commands.empty()) {
+
+            // 只有请求退出，并且命令队列处理完之后才退出循环。
+            if (m_shutdownRequest && m_commands.empty()) {
                 break;
             }
-        
             command = std::move(m_commands.front());
             m_commands.pop();
-        } // 退出临界区
+        }
 
-        try {
-            command(*cameraDevice);
+        CameraResult result;
+        try{
+            result = command.action(*cameraDevice);
+            command.promiseResult.set_value(std::move(result));   // promise异步修改命令执行结果
         } catch(...) {
 
         }
-    }
-
-    // 线程退出，注销采集回调函数
-    try {
-        cameraDevice->setFrameCallback({});
-    } catch(...) {
 
     }
     
-    // 如果正在采集，先停止采集
-    if (m_state.load() == State::Capturing) {
-        try {
-            CameraResult result = cameraDevice->stopCapture();
-            if (result.succeeded) {
-                m_state.store(State::Opened);
-            }
+    // 此时请求退出，并且任务队列中的任务已经处理完成，关闭相机设备
+    try {
+        cameraDevice->setFrameCallback(ICameraDevice::FrameCallback{});
+        cameraDevice->stopCapture();
+        cameraDevice->close();
+    } catch(...) {
 
-        } catch(...) {
-
-        }
-    }
-
-    // 如果没有关闭，则关闭设备
-    if (m_state.load() != State::Closed) {
-        try {
-            CameraResult result = cameraDevice->close();
-            if (result.succeeded) {
-                m_state.store(State::Closed);
-            }
-        } catch(...) {
-
-        }
-    }
-
-}
-
-
-void CameraCaptureService::shutdown() {
-    {
-        std::lock_guard<std::mutex> lock(m_mutex);
-        m_shutdownRequested = true;
-    }
-    m_condition.notify_one();
-
-    if (m_controlThread.joinable()) {
-        m_controlThread.join();
     }
 }
 
-CameraCaptureService::State CameraCaptureService::state() const noexcept {
-    return m_state;
+CameraCaptureService::State CameraCaptureService::state() const {
+    return  m_state.load();
 }
 
-bool CameraCaptureService::requestOpen() {
-    return post([this](ICameraDevice &cameraDevice) {
-        // 已经打开或正在录制，不需要再继续打开
-        if (m_state.load() == State::Opened || m_state.load() == State::Capturing) {
-            return;
+std::future<CameraResult> CameraCaptureService::requestOpen() {
+    return submit([this](ICameraDevice &cameraDevice)->CameraResult {
+        if (m_state.load() != State::Closed) {
+            return failure("相机已打开，忽略重复打开命令");
         }
-
         CameraResult result = cameraDevice.openFirstCamera();
         if (result.succeeded) {
-            m_state.store(State::Opened);
+            m_state.store(State::Opened);;
         }
+        return result;
     });
 }
-bool CameraCaptureService::requestClose() {
-    return post([this](ICameraDevice &cameraDevice) {
-        // 如果已经关闭，则不需要关闭
+
+std::future<CameraResult> CameraCaptureService::requestClose() {
+    return submit([this](ICameraDevice &cameraDevice)->CameraResult {
         if (m_state.load() == State::Closed) {
-            return;
+            return failure("相机已关闭，忽略重复关闭命令");
         }
-
-        // 如果正在采集，停止采集
-        if (m_state.load() == State::Capturing) {
+        if (m_state.load() == State::Captured) {
             CameraResult result = cameraDevice.stopCapture();
-            if (!result.succeeded) {
-                return;
-            }
-            m_state.store(State::Opened);
-        }
-
-        // 如果正在打开则关闭
-        if (m_state.load() == State::Opened) {
-            CameraResult result = cameraDevice.close();
             if (result.succeeded) {
-                m_state.store(State::Closed);
+                m_state.store(State::Opened);
+            } else {
+                return failure("关闭相机时无法停止捕获流");
             }
         }
+        CameraResult result = cameraDevice.close();
+        if (result.succeeded) {
+            m_state.store(State::Closed);
+        }
+        return result;
     });
 }
-bool CameraCaptureService::requestStartCapture() {
-    return post([this](ICameraDevice &cameraDevice) {
-        if (m_state.load() != State::Opened) {
-            return;
+
+std::future<CameraResult> CameraCaptureService::requestStartCapture() {
+    return submit([this](ICameraDevice &cameraDevice)->CameraResult {
+        if (m_state.load() == State::Closed) {
+            return failure("打开帧捕获流失败，请先打开相机");
+        }
+        if (m_state.load() == State::Captured) {
+            return failure("帧捕获流已经打开，忽略重复指令");
         }
         CameraResult result = cameraDevice.startCapture();
         if (result.succeeded) {
-            m_state.store(State::Capturing);
+            m_state = State::Captured;
         }
+        return result;
     });
 }
-bool CameraCaptureService::requestStopCapture() {
-    return post([this](ICameraDevice &cameraDevice) {
-        if (m_state.load() != State::Capturing) {
-            return;
+
+std::future<CameraResult> CameraCaptureService::requestStopCaptrue() {
+    return submit([this](ICameraDevice &cameraDevice)->CameraResult {
+        if (m_state.load() != State::Captured) {
+            return failure("关闭帧捕获流失败，帧捕获流已停止");
         }
         CameraResult result = cameraDevice.stopCapture();
         if (result.succeeded) {
             m_state.store(State::Opened);
         }
-    });
-}
-bool CameraCaptureService::requestSetFrameCallback(ICameraDevice::FrameCallback callback) {
-    return post([callback = std::move(callback)](ICameraDevice &cameraDevice) mutable {
-        cameraDevice.setFrameCallback(std::move(callback));
+        return result;
     });
 }
 
+std::future<CameraResult> CameraCaptureService::requestSetFrameCallback(ICameraDevice::FrameCallback callback) {
+        return submit([callback = std::move(callback)](ICameraDevice& cameraDevice) mutable {
+            /*
+             * ICameraDevice::setFrameCallback()返回void。
+             *
+             * 如果调用过程没有抛出异常，就由Application
+             * 构造一个成功的CameraResult。
+             */
+            cameraDevice.setFrameCallback(
+                std::move(callback)
+            );
 
-} // namespace learnopengl::application
+            return succeed();
+        }
+    );
+}
+
+
+} //learnopengl::application
